b4b60da921715aa4909156c14b8471fff1928e28
DuvanDiaz <DuvanDiaz@users.github.com>
2020-08-13 18:50:59 -0500

    third

--- mars_app.py
+++ mars_app.py
@@ -1,27 +1,45 @@
-from flask import Flask, render_template, redirect
-import pymongo
+import sys
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
 import scrape_mars
 
-# use pymongo to connect with mongodb server
-client = pymongo.MongoClient('mongodb://localhost:27017')
-db = client.mars_db
-collection = mars_db
-# Flask instance
+sys.setrecursionlimit(2000)
 app = Flask(__name__)
 
 
+# use pymongo to connect with mongodb server
+# client = pymongo.MongoClient('mongodb://localhost:27017')
+# db = client.mars_db
+# collection = mars_db
+
+
+
 # render index.html
-@app.route('/')
-def home():
-    mars = collection.find_one()
+# @app.route('/')
+# def home():
+#     mars = collection.find_one()
+#     return render_template("index.html", mars = mars)
+
+# @app.route('/scrape')
+# def scrape():
+#     scrape_mars.scrape()
+#     return redirect('/', code = 302)
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars_db.find_one()
     return render_template("index.html", mars = mars)
 
-@app.route('/scrape')
+@app.route("/scrape")
 def scrape():
-    scrape_mars.scrape()
-    return redirect('/', code = 302)
+    mars = mongo.db.mars_db 
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("http://localhost:27017/", code=302)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
